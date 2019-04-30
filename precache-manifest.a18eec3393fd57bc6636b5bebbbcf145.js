c774096bcb251f1a31ea890be22a98b79395cb19
Kim Flournoy <kimflournoy@gmail.com>
2019-04-29 21:50:33 -0400

    Updates

---
+++ precache-manifest.a18eec3393fd57bc6636b5bebbbcf145.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "148b3cab386bfde0d1fa",
+    "url": "/sf_challenge/static/js/runtime~main.9eaf4d3d.js"
+  },
+  {
+    "revision": "b7c631330af7cb7d89b2",
+    "url": "/sf_challenge/static/js/main.5f8b1b6f.chunk.js"
+  },
+  {
+    "revision": "c195a5d5f747f1f68ba4",
+    "url": "/sf_challenge/static/js/2.4e65827f.chunk.js"
+  },
+  {
+    "revision": "b7c631330af7cb7d89b2",
+    "url": "/sf_challenge/static/css/main.fb6b7d2c.chunk.css"
+  },
+  {
+    "revision": "5b96cf2379f0b70650dc99650436b8d6",
+    "url": "/sf_challenge/index.html"
+  }
+];
