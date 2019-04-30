045f145763f0db7c05d89afbf213234b1295ab89
Kim Flournoy <kimflournoy@gmail.com>
2019-04-30 00:14:35 -0400

    Cleaning up styles for shopping list items and cart total

--- src/components/ShoppingListItem/ShoppingListItem.js
+++ src/components/ShoppingListItem/ShoppingListItem.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import './ShoppingListItem.scss';
+import ItemImage from './item-image.png';
+import DeleteImage from './delete-image.svg';
 
 
 export class ShoppingListItem extends Component {
@@ -31,12 +33,17 @@ export class ShoppingListItem extends Component {
 
     return (
       <li className={"shoppinglistitem__list-item" + this.getStyle(completed)}>
+
+        {/* ToDo: make image dynamic */}
+        <img src={ItemImage} alt="" className="shoppinglistitem__item-image" />
         <input type="checkbox" className="shoppinglistitem__checkbox" id={"shoppinglistitem__checkbox_checkbox-" + id} onChange={this.props.toggleComplete.bind(this, id)} />
         <label htmlFor={"shoppinglistitem__checkbox_checkbox-" + id} className="shoppinglistitem__checkbox-label">
           <span className="shoppinglistitem__checkbox-label-title">{title}</span>
           <span className="shoppinglistitem__checkbox-label-stock">({stockMessage})</span>
         </label>
-        <button className="shoppinglistitem__delete-button" onClick={this.props.deleteListItem.bind(this, id)}>X</button>
+        <button className="shoppinglistitem__delete-button" onClick={this.props.deleteListItem.bind(this, id)}>
+          <img src={DeleteImage} alt="Delete Item" className="shoppinglistitem__delete-button-image" />
+        </button>
       </li>
     )
   }
